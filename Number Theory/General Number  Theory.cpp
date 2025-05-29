af0186124c21724ad1b666e6c86fc2b8088d0009
Punit-CP <164597761+Punit-CP@users.noreply.github.com>
2025-05-29 23:10:29 +0530

    Create General Number  Theory.cpp

---
+++ Number Theory/General Number  Theory.cpp
@@ -0,0 +1,49 @@
+// Some important points:
+The Euclidean algorithm is based on the next fact: if a≥b then gcd(a,b)=gcd(a−b,b)
+
+
+
+
+// 1.
+int phi(int n) {
+    int result = n;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            while (n % i == 0)
+                n /= i;
+            result -= result / i;
+        }
+    }
+    if (n > 1)
+        result -= result / n;
+    return result;
+}
+
+// 2. Euler Totient from 1 to n
+// TC: nloglogn
+void phi_1_to_n(int n) {
+    vector<int> phi(n + 1);
+    for (int i = 0; i <= n; i++)
+        phi[i] = i;
+
+    for (int i = 2; i <= n; i++) {
+        if (phi[i] == i) {
+            for (int j = i; j <= n; j += i)
+                phi[j] -= phi[j] / i;
+        }
+    }
+}
+
+// 3. Finding the totient from 1 to n using the divisor sum property --> E(p1)+E(p2)+E(p3)+E(p4)..........E(n) = n
+// TC: nlongn
+void phi_1_to_n(int n) {
+    vector<int> phi(n + 1);
+    phi[0] = 0;
+    phi[1] = 1;
+    for (int i = 2; i <= n; i++)
+        phi[i] = i - 1;
+
+    for (int i = 2; i <= n; i++)
+        for (int j = 2 * i; j <= n; j += i)
+              phi[j] -= phi[i];
+}
